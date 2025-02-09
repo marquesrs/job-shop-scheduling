5852c26f180305f7eecf5b1eadb0f8ed8234b1de
P Marques <13932504-psmbrs@users.noreply.gitlab.com>
2025-02-09 16:22:12 -0300

    temos log

--- main.odin
+++
@@ -1,9 +0,0 @@
-package main
-
-import "core:math/rand"
-import "core:fmt"
-
-
-main :: proc(){
-    fmt.println(rand.float64_von_mises(10, 1.5))
-}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use rand::{random_range, rng, Rng};
+use rand::{random_range, Rng};
+use std::fs::{self, File, OpenOptions};
+use std::path::Path;
+use std::{io, time};
+use std::io::prelude::*;
+use std::time::Instant;
+use std::fs::metadata;
 
 type Task = u32;
-
-type MachineId = usize;
 
 pub struct Machine {
     tasks: Vec<Task>,
@@ -41,15 +45,15 @@ impl MachineGroup {
         return group;
     }
 
-    pub fn group_makespan(&self) -> u32 {
+    pub fn group_max_makespan(&self) -> usize {
         let mut max_span = 0;
         for mach in &self.machines {
             max_span = std::cmp::max(max_span, mach.makespan());
         }
-        return max_span;
+        return max_span as usize;
     }
 
-    pub fn max_makespan_machine(&self) -> MachineId {
+    pub fn max_makespan_machine(&self) -> usize {
         let mut max = 0;
         let mut max_idx = 0;
         for i in 0..self.machines.len() {
@@ -61,7 +65,7 @@ impl MachineGroup {
         return max_idx;
     }
 
-    pub fn min_makespan_machine(&self) -> MachineId {
+    pub fn min_makespan_machine(&self) -> usize {
         let mut min = u32::MAX;
         let mut min_idx = 0;
         for i in 0..self.machines.len() {
@@ -73,29 +77,7 @@ impl MachineGroup {
         return min_idx;
     }
 
-    /*
-        TODO:
-        Sempre retorna o vizinho à direita.
-        Considerar retornar o vizinho mais livre ou mesmo ambos os vizinhos.
-    */
-    pub fn neighbor_id(&self, target: MachineId) -> Option<MachineId> {
-        let neighbor = target + 1;
-        if neighbor > self.machines.len() {
-            return None;
-        } else {
-            return Some(neighbor);
-        }
-    }
-
-    pub fn peek_last_task(&self, mach_id: MachineId) -> Option<Task> {
-        let n = self.machines[mach_id].tasks.len();
-        if n == 0 {
-            return None;
-        }
-        return Some(self.machines[mach_id].tasks[n - 1]);
-    }
-
-    pub fn select_neighbor_rng(&self, mach_id: MachineId) -> MachineId {
+    pub fn select_neighbor_rng(&self, mach_id: usize) -> usize {
         assert!(self.machines.len() > 1, "Cannot pick random neighbor without at least 2 machines");
         loop {
             let neighbor = random_range(0..self.machines.len());
@@ -105,7 +87,7 @@ impl MachineGroup {
         }
     }
 
-    pub fn peek_highest_task(&self, mach_id: MachineId) -> Option<(Task, usize)> {
+    pub fn peek_highest_task(&self, mach_id: usize) -> Option<(Task, usize)> {
         let n = self.machines[mach_id].tasks.len();
         if n == 0 {
             return None;
@@ -122,35 +104,21 @@ impl MachineGroup {
         return Some((self.machines[mach_id].tasks[max_idx], max_idx));
     }
 
-    fn pop_last_task(&mut self, mach_id: MachineId) -> Option<Task> {
-        return self.machines[mach_id].tasks.pop();
-    }
-
-    fn pop_task(&mut self, mach_id: MachineId, task_id: usize) -> Option<Task> {
+    fn pop_task(&mut self, mach_id: usize, task_id: usize) -> Option<Task> {
         if task_id >= self.machines[mach_id].tasks.len() {
             return None;
         }
         return Some(self.machines[mach_id].tasks.swap_remove(task_id));
     }
 
-    fn push_task(&mut self, mach_id: MachineId, task: Task) {
+    fn push_task(&mut self, mach_id: usize, task: Task) {
         self.machines[mach_id].tasks.push(task);
-    }
-
-    pub fn transfer_last_task(&mut self, source_id: MachineId, dest_id: MachineId) -> bool {
-        match self.pop_last_task(source_id) {
-            Some(e) => {
-                self.push_task(dest_id, e);
-                true
-            }
-            None => false,
-        }
     }
 
     pub fn transfer_task(
         &mut self,
-        source_id: MachineId,
-        dest_id: MachineId,
+        source_id: usize,
+        dest_id: usize,
         task_id: usize,
     ) -> bool {
         match self.pop_task(source_id, task_id) {
@@ -164,7 +132,7 @@ impl MachineGroup {
 }
 
 pub fn display_group(group: &MachineGroup) {
-    println!("group makespan: {}", group.group_makespan());
+    println!("Group Max Makespan: {}", group.group_max_makespan());
     for mach_id in 0..group.machines.len() {
         print!(
             "Makespan: {} M{}: ",
@@ -174,12 +142,12 @@ pub fn display_group(group: &MachineGroup) {
         for task in &group.machines[mach_id].tasks {
             print!("{} ", task);
         }
-        println!("\n");
+        println!("");
     }
 }
 
 pub fn display_info(
-    machine: MachineId,
+    machine: usize,
     makespan: u32,
     task: u32,
     neighbor_id: usize,
@@ -201,12 +169,14 @@ pub fn display_info(
 // TASK.TIME + MAC_VIZINHA.MAKESPAN < MAKESPAN
 // TRUE: TRANSFIRO A TASK PARA A MÁQUINA VIZINHA
 // FALSE: ENCERRA O LOOP
-fn local_search_best(mg: &mut MachineGroup) {
+fn local_search_best(mg: &mut MachineGroup, print_info: bool) -> usize {
     if mg.machines.len() < 2 {
-        return;
+        return 0;
     }
-
+    let mut iter = 0;
     loop {
+        iter = iter + 1;
+
         let source_id = mg.max_makespan_machine();
 
         let (task, task_id) = match mg.peek_highest_task(source_id) {
@@ -221,33 +191,35 @@ fn local_search_best(mg: &mut MachineGroup) {
         let dest_makespan = mg.machines[dest_id].makespan();
 
         if task + dest_makespan < source_makespan {
-            display_info(source_id, source_makespan, task, dest_id, dest_makespan);
+            if print_info {
+                display_info(
+                    source_id, 
+                    source_makespan, 
+                    task, 
+                    dest_id, 
+                    dest_makespan
+                );
+            }
             mg.transfer_task(source_id, dest_id, task_id);
         } else {
             break;
         }
     }
+    return iter;
 }
 
-// PEGO A MÁQUINA COM MAIOR MAKESPAN (1)
-// GUARDO O MAKESPAN (16)
-// PEGO UMA TASK DO TOPO (5)
-// TASK.TIME(5) + MAC_VIZINHA.MAKESPAN(0) < MAKESPAN(16)
-// TRUE: TRANSFIRO A TASK PARA A MÁQUINA VIZINHA
-// FALSE: ENCERRA O LOOP
-fn local_search_first(mg: &mut MachineGroup) {
-    todo!("Sera?");
-}
-
-type Real = f64;
-
-fn temperature(exec_progress: Real) -> Real {
+fn temperature(exec_progress: f64) -> f64 {
     const FALLOFF : f64 = 0.8;
     let t = - exec_progress + 1.0;
     return t;
 }
 
-fn accept_probability(current_makespan: u32, neighbor_makespan: u32, k: usize, max_k: usize) -> Real {
+fn accept_probability(
+    current_makespan: u32, 
+    neighbor_makespan: u32, 
+    k: usize, 
+    max_k: usize
+) -> f64 {
     let exec_progress = (k as f64) / (max_k as f64);
 
     if current_makespan > neighbor_makespan {
@@ -258,19 +230,101 @@ fn accept_probability(current_makespan: u32, neighbor_makespan: u32, k: usize, m
     }
 }
 
+fn log_string(
+    is_blm: bool,
+    n: usize,
+    m: usize,
+    rep: usize,
+    time: usize,
+    iter: usize,
+    makespan: usize,
+    parameter: usize,
+) -> String {
+    let heuristic = if is_blm {
+        "Local Search Best Improvement"
+    } else { "Simulated Annealing" };
+    let parameter = if is_blm {String::from("0")} else {parameter.to_string()};
+
+    return format!("{heuristic}, {n}, {m}, {rep}, {time}, {iter}, {makespan}, {parameter}\n");
+}
+
+fn write_log(log: Vec<String>) {
+    let file_path = "log.txt";
+    let file_exists = Path::new(file_path).exists();
+
+    let mut file = OpenOptions::new()
+        .append(true)    
+        .create(true)    
+        .open(file_path)
+        .expect("BURRO");
+
+    let file_metadata = fs::metadata(file_path).expect("BURRO");
+
+    if !file_exists || file_metadata.len() == 0 {
+        let header = "heuristica, n, m, replicacao, tempo, iteracoes, valor, parametro\n";
+        file.write_all(header.as_bytes()).expect("BURRO");
+    }
+
+    for record in log {
+        file.write_all(record.as_bytes()).expect("BURRO");
+    }
+    
+}
+
 pub fn main() {
+    const M: [usize; 3] = [10, 20, 50];
+    const R: [f32; 2] = [1.5, 2.0];
+    const IS_BLM: bool = true;
+    const PRINT_INFO: bool = false;
+    
+    let mut log: Vec<String> = Vec::new();
+
     let mut rng = rand::rng();
 
+    let mut group = MachineGroup::new(1);
+    
+    'outer: for m in M {
+        group = MachineGroup::new(m);
+        for r in R {
+            for rep in 0..10 {
+                let m = m as f32;
+                let n = m.powf(r).ceil() as usize;
+                for _ in 0..n {
+                    group.push_task(0, rng.random_range(1..=100) as Task);
+                }
+                let mut iter: usize = 0;
+                let time_now = time::Instant::now();
+                if IS_BLM { 
+                    iter = local_search_best(&mut group, PRINT_INFO); 
+                }
+                else {
+                }
+                let elapsed = time_now.elapsed().as_millis();
+                if PRINT_INFO { display_group(&group); }
+                log.push(log_string(
+                    IS_BLM, 
+                    n, 
+                    m as usize, 
+                    rep, 
+                    elapsed as usize, 
+                    iter, 
+                    group.group_max_makespan(),
+                    0,
+                ));
+                //break 'outer;
+            }
+        }
+    }
+    
+    write_log(log);
+    /*
+    let mut rng = rand::rng();
     let mut group = MachineGroup::new(10);
-
     for _ in 0..32 {
         group.push_task(0, rng.random_range(1..=30));
     }
-
+    local_search_best(&mut group, false);
     display_group(&group);
-
-    //local_search_first(&mut group);
-    local_search_best(&mut group);
-
-    display_group(&group);
+    write_log();
+    */
 }
